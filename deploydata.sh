c03714a86e70a2aeedfe2c1de354a55236ebe156
Nabil DJIDI <public@djidi.com>
2016-10-27 17:31:25 +0100

    roll-back

--- deploydata.sh
+++ deploydata.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 if [ -z $1 ] ; then read -p "Enter website local folder name: " FOLDER_NAME; else FOLDER_NAME=$1; fi
-if [ ! -z $2 ] ; then DB_NAME_ORIG=$2; elif [ ! -z $1 ] ; then DB_NAME_ORIG=$1 ; else read -p "Enter mysql data base name: " DB_NAME_ORIG; fi
-DB_NAME=$(echo $DB_NAME_ORIG | sed "s/-/_/g")
+if [ ! -z $2 ] ; then DB_NAME=$2; elif [ ! -z $1 ] ; then DB_NAME=$1 ; else read -p "Enter mysql data base name: " DB_NAME; fi
 read -p "Enter website domain: " DOMAIN_NAME;
 
 mkdir -p ~/export
-if $(mysqldump -hlocalhost -uroot -padmin16idc $DB_NAME_ORIG > ~/export/$DB_NAME.sql); then
+if $(mysqldump -hlocalhost -uroot -padmin16idc $DB_NAME > ~/export/$DB_NAME.sql); then
   sed -i -e "s/http:\/\/localhost\/${FOLDER_NAME}/http:\/\/$DOMAIN_NAME/g" ~/export/$DB_NAME.sql
   tar cvzf ~/export/$DB_NAME.sql.tar.gz -C ~/export $DB_NAME.sql
   rm ~/export/$DB_NAME.sql
   echo "base de donnée sauvegardée"
 else
-  echo "la base de donnée $DB_NAME_ORIG n'a pu être sauvegardée" && exit 1
+  echo "la base de donnée $DB_NAME n'a pu être sauvegardée" && exit 1
 fi
 [ ! -d "/var/www/${FOLDER_NAME}/IMG" ] && { echo "Le repertoire /var/www/$FOLDER_NAME/IMG n'existe pas" && exit 1; }
 tar cvzf ~/export/${FOLDER_NAME}_IMG.tar.gz -C /var/www/${FOLDER_NAME} IMG
